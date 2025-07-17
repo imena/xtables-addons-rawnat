bbafe4c49ddd92369cd5c94d6328097402e8841b
exsver <exsver@gmail.com>
2025-07-17 13:32:00 +0300

    u22 total rebuild

--- extensions/iptable_rawpost.c
+++ extensions/iptable_rawpost.c
@@ -1,6 +1,5 @@
 /*
  * 'rawpost' table, which is the very last hooked in at POST_ROUTING.
- * Copyright (C) 2016 Vittorio Gambaletta <linuxbugs@vittgam.net>
  *
  * Based on iptable_raw.c
  * Copyright (C) 2003 Jozsef Kadlecsik <kadlec@blackhole.kfki.hu>
@@ -8,19 +7,11 @@
  * https://github.com/torvalds/linux/blob/master/net/ipv4/netfilter/iptable_raw.c
  * https://github.com/torvalds/linux/blob/master/net/ipv4/netfilter/iptable_nat.c
  */
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 #include <linux/module.h>
 #include <linux/netfilter_ipv4/ip_tables.h>
 #include <linux/slab.h>
-#include <linux/version.h>
-#include <net/net_namespace.h>
-#include <net/netns/generic.h>
 #include <net/ip.h>
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
-static int iptable_rawpost_table_init(struct net *net);
-#else
-static int __net_init iptable_rawpost_table_init(struct net *net);
-#endif
 
 static const struct xt_table packet_rawpost = {
 	.name = "rawpost",                           /* A unique name. Up to 32 chars */
@@ -28,109 +19,66 @@ static const struct xt_table packet_rawpost = {
 	.me = THIS_MODULE,                           /* Set this to THIS_MODULE if you are a module, otherwise NULL */
 	.af = NFPROTO_IPV4,                          /* address/protocol family (IPv4) */
 	.priority = NF_IP_PRI_LAST,                  /* hook order */
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 15, 0)
-	.table_init = iptable_rawpost_table_init,
-#endif
 };
 
-static int rawpost_net_id;
-static inline struct xt_table **rawpost_pernet(struct net *net)
-{
-	return net_generic(net, rawpost_net_id);
-}
-
 /* The work comes in here from netfilter.c. */
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
 static unsigned int iptable_rawpost_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
 {
-    struct xt_table *table = *rawpost_pernet(state->net);
-
-    if (!table)
-        return NF_ACCEPT;
-
-    if (state->hook == NF_INET_POST_ROUTING && (skb->len < sizeof(struct iphdr) || ip_hdrlen(skb) < sizeof(struct iphdr)))
-        return NF_ACCEPT;
-
-    return ipt_do_table(skb, state, table);
+	return ipt_do_table(skb, state, priv);
 }
-#else
-static unsigned int iptable_rawpost_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
-{
-	if (state->hook == NF_INET_POST_ROUTING && (skb->len < sizeof(struct iphdr) || ip_hdrlen(skb) < sizeof(struct iphdr)))
-		/* root is playing with raw sockets. */
-		return NF_ACCEPT;
-
-	return ipt_do_table(skb, state, *rawpost_pernet(state->net));
-}
-#endif
 
 static struct nf_hook_ops *rawposttable_ops __read_mostly;
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
 static int iptable_rawpost_table_init(struct net *net)
-#else
-static int __net_init iptable_rawpost_table_init(struct net *net)
-#endif
 {
-	struct xt_table **iptable_rawpost = rawpost_pernet(net);
 	struct ipt_replace *repl;
+	const struct xt_table *table = &packet_rawpost;
 	int ret;
 
-	if (*iptable_rawpost)
-		return 0;
-
-	repl = ipt_alloc_initial_table(&packet_rawpost);
+	repl = ipt_alloc_initial_table(table);
 	if (repl == NULL)
 		return -ENOMEM;
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
-	ret = ipt_register_table(net, &packet_rawpost, repl, rawposttable_ops);
+	ret = ipt_register_table(net, table, repl, rawposttable_ops);
 	kfree(repl);
 	return ret;
-#else
-	ret = ipt_register_table(net, &packet_rawpost, repl, rawposttable_ops, iptable_rawpost);
-	kfree(repl);
-	return ret;
-#endif
+}
+
+static void __net_exit iptable_rawpost_net_pre_exit(struct net *net)
+{
+	ipt_unregister_table_pre_exit(net, "rawpost");
 }
 
 static void __net_exit iptable_rawpost_net_exit(struct net *net)
 {
-	struct xt_table **iptable_rawpost = rawpost_pernet(net);
-	if (!*iptable_rawpost)
-		return;
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
-    ipt_unregister_table_pre_exit(net, "rawpost");
-#else
-	ipt_unregister_table(net, *iptable_rawpost, rawposttable_ops);
-#endif
-	*iptable_rawpost = NULL;
+	ipt_unregister_table_exit(net, "rawpost");
 }
 
 static struct pernet_operations iptable_rawpost_net_ops = {
+	.pre_exit = iptable_rawpost_net_pre_exit,
 	.exit = iptable_rawpost_net_exit,
-	.id   = &rawpost_net_id,
-	.size = sizeof(struct xt_table *),
 };
 
 static int __init iptable_rawpost_init(void)
 {
 	int ret;
+	const struct xt_table *table = &packet_rawpost;
 
-	/* Register hooks */
-	rawposttable_ops = xt_hook_ops_alloc(&packet_rawpost, iptable_rawpost_hook);
-	if (IS_ERR(rawposttable_ops))
+	ret = xt_register_template(table,
+				   iptable_rawpost_table_init);
+	if (ret < 0)
+		return ret;
+
+	rawposttable_ops = xt_hook_ops_alloc(table, iptable_rawpost_hook);
+	if (IS_ERR(rawposttable_ops)) {
+		xt_unregister_template(table);
 		return PTR_ERR(rawposttable_ops);
+	}
 
 	ret = register_pernet_subsys(&iptable_rawpost_net_ops);
 	if (ret < 0) {
+		xt_unregister_template(table);
 		kfree(rawposttable_ops);
 		return ret;
-	}
-
-	ret = iptable_rawpost_table_init(&init_net);
-	if (ret) {
-		unregister_pernet_subsys(&iptable_rawpost_net_ops);
-		kfree(rawposttable_ops);
 	}
 
 	return ret;
@@ -140,8 +88,10 @@ static void __exit iptable_rawpost_fini(void)
 {
 	unregister_pernet_subsys(&iptable_rawpost_net_ops);
 	kfree(rawposttable_ops);
+	xt_unregister_template(&packet_rawpost);
 }
 
 module_init(iptable_rawpost_init);
 module_exit(iptable_rawpost_fini);
 MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("iptables legacy rawpost table");
